dc3cc32757a6999ac755d5684931f1608e2ba6ae
Nemo Semret <nemo@semret.org>
2025-04-14 23:53:12 +0300

    add anonymous user read-only access for search

--- tools/conf/setup_elasticsearch.sh
+++ tools/conf/setup_elasticsearch.sh
@@ -10,7 +10,7 @@ curl --user elastic:$ELASTIC_PASSWORD  -X PUT http://localhost:9200/_security/ro
       "privileges": ["write", "create", "index", "auto_configure","create_index","manage","all"]
     }
   ],
-  "description": "Role for Logstash to write to qrb_mon index"  
+  "description": "Role for Logstash to write to qrb_mon index"
 }
 '
 
@@ -18,19 +18,42 @@ curl --user elastic:$ELASTIC_PASSWORD -X POST "localhost:9200/_security/user/log
 {
   "password" : "sumdumting",
   "roles" : [ "logstash_writer" ],
-  "full_name": "QRB Labs logstash",
-  "email": "ops+logstash@qrb-labs.com"
 }
 '
 
 curl --user elastic:$ELASTIC_PASSWORD -X POST "localhost:9200/_security/role_mapping/mapping1?pretty" -H 'Content-Type: application/json' -d'
 {
   "roles": [ "logstash_writer"],
-  "enabled": true, 
+  "enabled": true,
   "rules": {
     "field" : { "username" : "logstash_writer" }
   },
-  "metadata" : { 
+  "metadata" : {
+    "version" : 1
+  }
+}
+'
+
+curl --user elastic:$ELASTIC_PASSWORD  -X PUT http://localhost:9200/_security/role/anonymous_search -H "Content-Type: application/json" -d'
+{
+  "cluster": ["monitor"],
+  "indices": [
+    {
+      "names": ["qrb_mon", "qrb_mon-*"],
+      "privileges": ["read"]
+    }
+  ]
+}
+'
+
+curl --user elastic:$ELASTIC_PASSWORD -X POST "localhost:9200/_security/role_mapping/mapping2?pretty" -H 'Content-Type: application/json' -d'
+{
+  "roles": [ "anonymous_search"],
+  "enabled": true,
+  "rules": {
+    "field" : { "username" : "*"}
+  },
+  "metadata" : {
     "version" : 1
   }
 }
