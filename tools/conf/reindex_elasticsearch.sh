3877e14c6067ae606a4bcd4330f6f1ffde9cf8e6
Nemo Semret <nemo@semret.org>
2025-08-30 03:25:37 +0000

    deduplicate miner status events that are repeated by api

--- tools/conf/reindex_elasticsearch.sh
+++ tools/conf/reindex_elasticsearch.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+# Script to convert old qrb_mon index (which didn't deduplicate repeated status from miner api to new one which does.
+# The filter in logstash.conf ensures new docs have an equivalent unique constraint
+
 curl --user elastic:$ELASTIC_PASSWORD  -X PUT http://localhost:9200/_reindex -H "Content-Type: application/json" -d'
 {
   "source": {
     "index": "qrb_mon"
   },
   "dest": {
-    "index": "qrb_mon_dedup"
-  }
+    "index": "qrb_mon-dedup2"
+  },
   "script": {
     "lang": "painless",
-    "source": "ctx._id = ctx.@timestamp +  '_' +  ctx.host.ip + '_' + ctx.code + '_' + ctx.message"
+    "source": "if(ctx._source.host != null && ctx._source.message != null) {ctx._id = ctx._source.timestamp + ctx._source.host.ip + ctx._source.code + ctx._source.message.sha256();} if(ctx._source.Slot != null) {ctx._id = ctx._id + ctx._source.Slot;}"
   }
 }
 '
