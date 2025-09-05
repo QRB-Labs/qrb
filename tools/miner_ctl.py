96f89d55515accb7bdb75c5b954bfd3548f26593
Nemo Semret <nemo@semret.org>
2025-09-05 21:42:56 +0000

    set power mode and reboot

--- tools/miner_ctl.py
+++ tools/miner_ctl.py
@@ -16,23 +16,36 @@ from pywhatsminer.core import WhatsminerAccessToken, WhatsminerAPI
 import miner_lib
 
 
+def whatsminer_reboot(access_token):
+    resp = WhatsminerAPI.exec_command(access_token, "reboot")
+    miner_lib.check_response(resp)
+    return resp
 
-def whatsminer_set_time_zone(password, ip, port=4028):
-    access_token = WhatsminerAccessToken(ip, port, password)
+
+def whatsminer_set_power_mode(access_token):
+    resp = WhatsminerAPI.exec_command(access_token, "set_normal_power")
+    miner_lib.check_response(resp)
+    return resp
+
+
+def whatsminer_set_time_zone(access_token):
     resp = WhatsminerAPI.exec_command(access_token, "set_zone", {"timezone": "UTC", "zonename": "Etc/UTC"})
-    logging.info(f"{ip}: {json.dumps(resp)}")
+    miner_lib.check_response(resp)
     return resp
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Tool to get error codes from miner APIs')
+    parser = argparse.ArgumentParser(description='Tool to send commands to miners write API')
     parser.add_argument("--start_ip", required=True)
-    parser.add_argument("--end_ip", required=True)
+    parser.add_argument("--end_ip", required=False)
     parser.add_argument("--password", required=True)
 
     args = parser.parse_args()
     start_ip_octets = args.start_ip.split('.')
-    end_ip_octets = args.end_ip.split('.')
+    if args.end_ip:
+        end_ip_octets = args.end_ip.split('.')
+    else:
+        end_ip_octets = start_ip_octets
     assert len(start_ip_octets) == 4
     assert len(end_ip_octets) == 4
 
@@ -44,6 +57,7 @@ def main():
                 for octet3 in range(int(start_ip_octets[3]), int(end_ip_octets[3])+1):
 
                     ip = '.'.join(map(str, [octet0, octet1, octet2, octet3]))
+                    access_token = WhatsminerAccessToken(ip, 4028, args.password)
 
                     try:
                         api_version = miner_lib.whatsminer_get_version(ip)
@@ -51,7 +65,10 @@ def main():
                         assert api_version['Msg']['api_ver'].split('.')[0] == '2', \
                             "Unsupported WhatsMiner API version {}".format(api_version['Msg']['api_ver'])
 
-                        whatsminer_set_time_zone(args.password, ip)
+                        whatsminer_set_time_zone(access_token)
+                        whatsminer_set_power_mode(access_token)
+                        whatsminer_reboot(access_token)
+                        
                     except OSError as e:
                         logging.error(f"{ip}: {e}")
                     except Exception as e2:
