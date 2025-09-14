b260a986749fe7a8960dff172380abe9f4c3403a
Nemo Semret <nemo@semret.org>
2025-09-14 14:27:04 +0000

    use qrb_logging lib

--- rpi/test.py
+++ rpi/test.py
@@ -1,34 +1,6 @@
-import logging
-from logstash import TCPLogstashHandler
-from logstash.formatter import LogstashFormatterBase
-import socket
+from qrb_logging import get_logger
 
-
-class LogstashFormatter(LogstashFormatterBase):
-    def format(self, record):
-        if isinstance(record.msg, dict):
-            message = record.msg
-            message.update({
-                'path': record.pathname,
-                'level': record.levelname,
-                'logger_name': record.name,
-            })
-        else:
-            message = {"@message" : record.msg}
-        message['@source_host'] = socket.gethostname()
-        return self.serialize(message)
-
-    
-def get_logger(logger_name):
-    my_logger = logging.getLogger(logger_name)
-    my_logger.setLevel(logging.DEBUG)
-    handler = TCPLogstashHandler(host='192.168.6.100', port=5959)
-    handler.setFormatter(LogstashFormatter()) 
-    my_logger.addHandler(handler)
-    return my_logger
-
-
-mylogger =  get_logger("rpi_testing")
+mylogger = get_logger("rpi_testing")
 mylogger.warning({"message": "Fake ON"})
 mylogger.info({"Temperature": 12.5})
 mylogger.error("plain error")
