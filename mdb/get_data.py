a5c55624b033a08215fcde0715ca7c491915e004
Nemo Semret <nemo@semret.org>
2026-02-02 17:52:19 +0000

    show error message to client when sync fails

--- mdb/get_data.py
+++ mdb/get_data.py
@@ -8,14 +8,15 @@ import tempfile
 def sync_sheets(json_key, spreadsheet_url, tab_names):
     gc = gspread.service_account(filename=json_key)
     sh = gc.open_by_url(spreadsheet_url)
+    temp_dir = tempfile.gettempdir()
 
     for name in tab_names:
+        print(f"Downloading {name} ...")
         worksheet = sh.worksheet(name)
         records = worksheet.get_all_records()
         df = pd.DataFrame(records)
         df = df.dropna(how='all')
 
-        temp_dir = tempfile.gettempdir()
         output_file = os.path.join(temp_dir, f'{name}.csv')
         df.to_csv(output_file)
         yield output_file
@@ -28,6 +29,6 @@ if __name__ == "__main__":
     parser.add_argument('--worksheets', nargs='+',
                         help='list of worksheet names eg --worksheets "Miners" "Other miners"',  default=["Miners"])
     args = parser.parse_args()
-    
+
     filenames = sync_sheets(args.key_file, args.sheet_url, args.worksheets)
     print(list(filenames))
